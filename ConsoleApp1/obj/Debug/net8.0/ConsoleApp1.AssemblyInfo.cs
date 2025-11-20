f4aab907f56079410a099f3c5e90ba1dc325c182
Tony Riad <92161477+T-King-00@users.noreply.github.com>
2025-11-20 14:18:05 +0100

    -integration tests are added .

--- ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a07908dfa13121c6c1449bab77521ff9f70b2e2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cfe0b75054c95bad44137b2e944c2eea20c343f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ IntegrationTesting/IntegrationTests.cs
@@ -0,0 +1,71 @@
+﻿namespace IntegrationTest;
+
+using NUnit.Framework.Constraints;
+using NUnit.Framework;
+using ConsoleApp1;
+
+// Integration tests 
+[TestFixture]
+public class IntegrationTests
+{
+
+    [OneTimeSetUp]
+    public void OneTimeSetup()
+    {
+        BankAccount.Intiate();
+        Console.WriteLine("onetime setup");
+    }
+
+
+    /* integration test case 1 :
+    Description : Test create account and logining in modules*/
+
+    [Test]
+    [Category("IntegrationTest")]
+    public static void Register_Login_ReturnsTrue()
+    {
+
+        string accountHolderName = "Andrew";
+        string accountHolderType = "Free";
+        float balance = 100f;
+        string validPin = "12346789";
+
+        //register
+        BankAccount newBankAccount = new BankAccount(accountHolderName, accountHolderType, balance, validPin);
+        Boolean result0 = BankAccount.RegisterACC(accountHolderName, accountHolderType, balance, validPin);
+        Assert.That(result0);
+        //login
+        bool result1 = BankAccount.Login(accountHolderName, validPin);
+
+        //ASSERT
+        Assert.That(result1);
+
+    }
+
+    /*integration test case 2 :
+     Description : Test logging in and depositing money */
+    [Test]
+    [Category("IntegrationTest")]
+    public void Login_DepositM_ReturnsTrue()
+    {
+        string accountHolderName = "mike";
+        string accountHolderType = "Free";
+        float balance = 1020f;
+        string validPin = "1234";
+        //login
+        BankAccount bankAcc_testObj = new BankAccount(accountHolderName, accountHolderType, balance, validPin);
+        bool result = BankAccount.Login(accountHolderName, validPin);
+        Assert.That(result);
+
+        //deposit amount of money.
+        float depositAmount = 1000;
+        bankAcc_testObj.DepositMoney(depositAmount);
+        float actualBalance = bankAcc_testObj.BalanceInquiry();
+        Assert.That(actualBalance, Is.EqualTo(balance + depositAmount));
+
+    }
+}
+
+
+
+
